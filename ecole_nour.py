95efa41eb9ee3e674ea207e8b2ca76f908ebf2c7
Mohamed Selim Alouini <alouiniselim@gmail.com>
2024-01-24 14:53:13 +0100

    Lup

--- ecole_nour.py
+++ ecole_nour.py
@@ -11,7 +11,15 @@ st.set_page_config(page_title="Input", page_icon="🏠️", layout="centered", i
 
 #pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-col = "text"
+def check(txt_extracted):
+    if txt_extracted is None or txt_extracted == "":
+            if genre == "عربي":
+                txt_extracted = "لا يوجد نص"
+            elif genre == "Français":
+                txt_extracted = "Il n'y a pas de texte"
+            else:
+                txt_extracted = "There is no text"
+    return txt_extracted
 
 genre = st.radio(
     "",
@@ -22,54 +30,47 @@ genre = st.radio(
 if genre == "عربي":
     lg = "ara"
     lg_mp3 = "ar"
+    confirm = "استمع"
 elif genre == "Français":
     lg = "fra"
     lg_mp3 = "fr"
+    confirm = "Ecoutez"
 else:
     lg = "eng"
     lg_mp3 = "en"
+    confirm = "Listen"
 
-def reset_doc():
-    if txt_extracted is not None:
-        del txt_extracted
 
-def reset_text():
-    if doc is not None:
-        del doc
-
-tab1, tab2 = st.tabs(["Text", "Pdf"])
+tab1, tab2 = st.tabs(["Text", "Document"])
 
 with tab1:
-    txt_extracted = st.text_area("text", on_change=reset_doc)
+    txt_extracted = st.text_area("text")
+    if st.button(confirm):
+        txt_extracted = check()
+        mp3_fp = BytesIO()
+        tts = gTTS(txt_extracted, lang=lg_mp3)
+        st.audio(mp3_fp, format='audio/wav')
+
 
 with tab2:    
-    doc = st.file_uploader("", type=["pdf", "docx", "doc"], accept_multiple_files=False, key=None, help=None, on_change=reset_text, args=None, kwargs=None, disabled=False, label_visibility="hidden")
-
-if doc is not None or txt_extracted is not None:
-    if doc is not None:
+    doc = st.file_uploader("Document", type=["pdf", "docx", "doc"], accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False, label_visibility="hidden")
+    if st.button(confirm):
         txt_extracted = ""
-        if doc.name.endswith('pdf'):
-            images = pdf.convert_from_bytes(doc.getvalue())
+        if doc is not None:
+            if doc.name.endswith('pdf'):
+                images = pdf.convert_from_bytes(doc.getvalue())
 
-            for im in images:
-                text = pytesseract.image_to_string(im, lang=lg)
+                for im in images:
+                    text = pytesseract.image_to_string(im, lang=lg)
+                    txt_extracted += text + "\n"
+        
+        txt_extracted = check()
 
-                txt_extracted += text + "\n"
-    
-    if txt_extracted is None or txt_extracted == "":
-        if genre == "عربي":
-            txt_extracted = "لا يوجد نص"
-        elif genre == "Français":
-            txt_extracted = "Il n'y a pas de texte"
-        else:
-            txt_extracted = "There is no text"
+        mp3_fp = BytesIO()
+        tts = gTTS(txt_extracted, lang=lg_mp3)
 
-    mp3_fp = BytesIO()
-    tts = gTTS(txt_extracted, lang=lg_mp3)
-    
-    if doc is not None:
         tts.write_to_fp(mp3_fp)
 
-    st.audio(mp3_fp, format='audio/wav')
-    st.write(txt_extracted)
+        st.audio(mp3_fp, format='audio/wav')
+        st.write(txt_extracted)
 
